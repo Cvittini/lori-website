72c00ec172cc43ff4d68d5b35092b98054e30379
Cristian A Vittini <140289468+Cvittini@users.noreply.github.com>
2025-08-13 17:53:07 -0400

    Configure Strapi CORS for front-end

--- lori-strapi/config/middlewares.ts
+++ lori-strapi/config/middlewares.ts
@@ -1,21 +1,17 @@
 // lori-strapi/config/middlewares.ts
 export default ({ env }) => [
   'strapi::errors',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: env.array('CORS_ORIGIN', [
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'http://localhost:3001',      // CRA sometimes bumps to 3001
-        'http://localhost:5173',      // Vite (if you ever switch)
-        // 'https://your-frontend-domain.com', // add prod domain
-      ]),
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      credentials: true,              // if true, origin cannot be "*"
+    {
+      name: 'strapi::cors',
+      config: {
+        origin: env.array('CORS_ORIGIN', [
+          'http://localhost:3000',      // front-end origin
+        ]),
+        headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+        credentials: true,              // if true, origin cannot be "*"
+      },
     },
-  },
   'strapi::security',
   'strapi::poweredBy',
   'strapi::logger',
