35fbd19bde02210419bd6f815d7d9bce9eb5cc29
Cvittini <cvittini@gmail.com>
2025-08-17 13:07:21 -0400

    eliminating env file

--- src/lib/email.js
+++ src/lib/email.js
@@ -16,3 +16,9 @@ export function sendEmail(params) {
 export function isValidEmail(value) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
 }
+
+console.log('ENV CHECK', {
+  SERVICE_ID: process.env.REACT_APP_EMAILJS_SERVICE_ID,
+  TEMPLATE_ID: process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+  PUBLIC_KEY: process.env.REACT_APP_EMAILJS_PUBLIC_KEY ? 'present' : 'MISSING'
+});
